3d6f06317e08f4f3aaad39e3306bd0e7e4d7a646
Chiu Tsui <chiutsui@MacBook-Pro.local>
2016-05-21 21:52:45 -0400

    Up to Music Player

--- app/src/main/java/com/example/chiutsui/sounddroid/MainActivity.java
+++ app/src/main/java/com/example/chiutsui/sounddroid/MainActivity.java
@@ -1,15 +1,24 @@
 package com.example.chiutsui.sounddroid;
 
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.chiutsui.sounddroid.soundcloud.SoundCloud;
 import com.example.chiutsui.sounddroid.soundcloud.SoundcloudService;
 import com.example.chiutsui.sounddroid.soundcloud.Track;
+import com.squareup.picasso.Picasso;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,22 +34,53 @@ public class MainActivity extends AppCompatActivity {
     private List<Track> mTracksRetrieved = null;
 
     private TracksAdapter mAdapter;
+    private ImageView mPlayerThumbnail;
+    private TextView mPlayerTitle;
 
-
+    private MediaPlayer mMediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.playerToolbar);
+
+        mMediaPlayer = new MediaPlayer();
+        mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        mMediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.start();
+            }
+        });
+
+        mPlayerThumbnail = (ImageView) findViewById(R.id.playerTrackThumbnail);
+        mPlayerTitle = (TextView) findViewById(R.id.playerTrackName);
+
         final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.songsList);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         mTracksRetrieved = new ArrayList<Track>();
         mAdapter = new TracksAdapter(this, mTracksRetrieved);
+        mAdapter.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Track selectedTrack = mTracksRetrieved.get(position);
+                mPlayerTitle.setText(selectedTrack.getmTitle());
+                Picasso.with(MainActivity.this).load(selectedTrack.getAvatarURL()).into(mPlayerThumbnail);
+
+                try {
+                    mMediaPlayer.setDataSource(selectedTrack.getmStreamURL()+"?client_id="+SoundcloudService.CLIENT_ID);
+                    mMediaPlayer.prepareAsync();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
         recyclerView.setAdapter(mAdapter);
 
 
-        SoundCloud.getService().searchSongs("dark horse", new Callback<List<Track>>() {
+        SoundCloud.getService().searchSongs("space man", new Callback<List<Track>>() {
             @Override
             public void success(List<Track> tracks, Response response) {
                 Log.d(TAG,"Length is "+tracks.size());
--- app/src/main/java/com/example/chiutsui/sounddroid/TracksAdapter.java
+++ app/src/main/java/com/example/chiutsui/sounddroid/TracksAdapter.java
@@ -5,6 +5,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -17,7 +18,7 @@ import java.util.List;
  * Created by chiutsui on 5/15/16.
  */
 public class TracksAdapter extends RecyclerView.Adapter<TracksAdapter.ViewHolder> {
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         private final TextView titleText;
         private final ImageView trackThumbnail;
 
@@ -25,11 +26,25 @@ public class TracksAdapter extends RecyclerView.Adapter<TracksAdapter.ViewHolder
             super(v);
             titleText = (TextView) v.findViewById(R.id.trackTitle);
             trackThumbnail = (ImageView) v.findViewById(R.id.track_thumbnail);
+            v.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            if (mOnItemClickListener != null) {
+                mOnItemClickListener.onItemClick(null,v,getAdapterPosition(),0);
+            }
         }
     }
 
     private List<Track> mTracks;
     private Context mContext;
+
+    private AdapterView.OnItemClickListener mOnItemClickListener;
+
+    public void setOnItemClickListener(AdapterView.OnItemClickListener onItemClickListener) {
+        mOnItemClickListener = onItemClickListener;
+    }
 
     TracksAdapter(Context context, List<Track> tracks) {
         mTracks=tracks;
